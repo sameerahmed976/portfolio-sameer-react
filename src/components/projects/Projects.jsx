21c8e2730c7b83fc5118cbb6fc39577ae22ff2f6
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2023-07-24 13:26:29 +0530

    added project github finder

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -40,6 +40,73 @@ const Projects = () => {
                 </div>
               </div>
             </article>
+
+            {/* cocktail shop search */}
+            <article className="projects__card">
+              <div className="projects__card__inner">
+                <div className="projects__card__front">
+                  <img
+                    src="/cocktail search.png"
+                    alt="project 2"
+                    className="projects__image"
+                  />
+                </div>
+                <div className="projects__card__back">
+                  <div className="projects__btn__group">
+                    <a
+                      href="https://cocktail-shop-react.vercel.app/"
+                      target="_blank"
+                      className="btn--demo"
+                      rel="noreferrer"
+                    >
+                      Live Demo
+                    </a>
+                    <a
+                      href="https://github.com/sameerahmed976/cocktail__shop__cart-react"
+                      target="_blank"
+                      className="btn--code"
+                      rel="noreferrer"
+                    >
+                      <i className="fab fa-github-square btn--github"></i>{" "}
+                      Source Code
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </article>
+            {/* github finder search */}
+            <article className="projects__card">
+              <div className="projects__card__inner">
+                <div className="projects__card__front">
+                  <img
+                    src="/cocktail search.png"
+                    alt="project 2"
+                    className="projects__image"
+                  />
+                </div>
+                <div className="projects__card__back">
+                  <div className="projects__btn__group">
+                    <a
+                      href="https://cocktail-shop-react.vercel.app/"
+                      target="_blank"
+                      className="btn--demo"
+                      rel="noreferrer"
+                    >
+                      Live Demo
+                    </a>
+                    <a
+                      href="https://github.com/sameerahmed976/cocktail__shop__cart-react"
+                      target="_blank"
+                      className="btn--code"
+                      rel="noreferrer"
+                    >
+                      <i className="fab fa-github-square btn--github"></i>{" "}
+                      Source Code
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </article>
           </section>
         </section>
       </section>
