4fdb110acebf49b61bfe45725742cefaa00af4f0
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2023-07-29 20:19:27 +0530

    added mern-project-workout-tracker

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,6 +8,49 @@ const Projects = () => {
         </section>
         <section className="section__center">
           <section className="projects__container">
+            {/* workout  mern  app*/}
+            <article className="projects__card">
+              <div className="projects__card__inner">
+                <div className="projects__card__front">
+                  <img
+                    src="/workout-tracker.png"
+                    alt="workout tracker"
+                    className="projects__image"
+                  />
+                </div>
+                <div className="projects__card__back">
+                  <div className="projects__btn__group">
+                    <a
+                      href="https://workout-tracker-ixpy.onrender.com"
+                      target="_blank"
+                      className="btn--demo"
+                      rel="noreferrer"
+                    >
+                      Live Demo
+                    </a>
+                    <a
+                      href=" https://github.com/sameerahmed976/workout-frontend"
+                      target="_blank"
+                      className="btn--code"
+                      rel="noreferrer"
+                    >
+                      <i className="fab fa-github-square btn--github"></i>{" "}
+                      Source Code -frontend
+                    </a>
+                    <a
+                      href="https://github.com/sameerahmed976/workout-backend"
+                      target="_blank"
+                      className="btn--code"
+                      rel="noreferrer"
+                    >
+                      <i className="fab fa-github-square btn--github"></i>{" "}
+                      Source Code -backend
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </article>
+
             {/* cocktail shop search */}
             <article className="projects__card">
               <div className="projects__card__inner">
